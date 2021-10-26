fa09ef92daf054f06a66c71ccfc9980c69606643
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-26 15:06:20 +0530

    Welcome to User Registration System

---
+++ src/system/users/UserRegistrationSystem.java
@@ -0,0 +1,11 @@
+package system.users;
+
+public class UserRegistrationSystem {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to User Registration Program");
+		
+	}
+
+}
