d579a356692035dca4603c4aa8f02d805ef0c267
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-26 17:44:50 +0530

    users first name validation is added

--- src/system/users/UserRegistrationSystem.java
+++ src/system/users/UserRegistrationSystem.java
@@ -5,7 +5,8 @@ public class UserRegistrationSystem {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to User Registration Program");
-		
+		ValidUserName validate = new ValidUserName();
+		validate.validateFirstName();
 	}
 
 }
---
+++ src/system/users/ValidUserName.java
@@ -0,0 +1,38 @@
+package system.users;
+
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+public class ValidUserName {
+	
+	// regex for the user First Name with 1st char Upper and minimum 3 chars
+	String regEx = "^[A-Z]{1}[a-zA-Z0-9]{2,}";
+	
+	Scanner input = new Scanner(System.in);
+
+	void validateFirstName() {
+		
+		System.out.println("Enter user's First Name");
+		String firstName = input.nextLine();
+		Pattern pattern = Pattern.compile(regEx);
+		
+		Predicate<String> match = pattern.asPredicate();
+		boolean result = match.test(firstName);
+		
+		if(result) {
+			System.out.println(firstName + " is Valid first Name");
+		}
+		else {
+			System.out.println(firstName + " is Invalid first Name");
+		}
+	}
+
+	public ValidUserName() {
+		super();
+		this.regEx = regEx;
+		this.input = input;
+	}
+	
+	
+}
